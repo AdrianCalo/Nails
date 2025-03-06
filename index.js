664099cf9f3dda4eb7077dadbf103febb0bf3253
Adrian Calo <110639622+AdrianCalo@users.noreply.github.com>
2025-03-05 21:06:44 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ function togglePasswordVisibility(fieldId){
         field.type='password'
     }
 };
+
+/*Carga categorias*/
+let categorias=["Esmaltes","Removedores","Decoracion","Herramientas","Equipos"]
+
+/*metodo para cargar categorias a la lista*/
+
+function CrearCategorias(array) {
+    /*obtner el contenedor donde se insertara la lsita*/
+    const contenedor= document.getElementById("listasContainer")
+    
+    //Creamos la lista desordenada
+    const ul= document.createElement("ul");
+    
+    //reecooremos el array y cremaos los elementos de la lista
+    array.forEach(item=>{
+        const li=document.createElement("li");
+        li.textContent=item;
+        ul.appendChild(li);
+    });
+    //limpiamos el contenedor antes de agregar la nueva lista
+
+    contenedor.innerHTML='';
+    contenedor.appendChild(ul);
+};
+
+//ejemplo de uso
+CrearCategorias(categorias)
